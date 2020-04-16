8f9c40033ff513508bb33f119b20bc455464f254
dkStephanos <stephanos@goldmail.etsu.edu>
2020-04-16 13:49:33 -0400

    Starting excercise 9

---
+++ Excercise9/excercise9.py
@@ -0,0 +1,12 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
+if __name__ == '__main__':
+    potentialClientsDF = pd.read_csv('PotentialClients.csv')
+
+    #Print preview of dataframe
+    print("\n1. Checking and Reading the Data\n")
+    print(potentialClientsDF.head())
