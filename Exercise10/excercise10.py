8acd5665b64dfb56e6da0fd2a7978f3173a46b72
dkStephanos <stephanos@goldmail.etsu.edu>
2020-04-16 14:58:50 -0400

    Finished, add method to plot clusters

--- Exercise10/excercise10.py
+++ Exercise10/excercise10.py
@@ -1,13 +1,17 @@
 import csv
 import pandas as pd
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from sklearn import metrics 
 from scipy.spatial.distance import cdist 
+from scipy import cluster
 from sklearn.cluster import KMeans
 
 if __name__ == '__main__':
+    #Read in raw data, casting to float
     potentialClientsDF = pd.read_csv('PotentialClients.csv')
+    potentialClientsDF = potentialClientsDF.astype('float64')
 
     #Print preview of dataframe
     print("\n1. Checking and Reading the Data\n")
@@ -18,30 +22,21 @@ if __name__ == '__main__':
     plt.title("Raw Potential Client Data")
     plt.show()
 
-    #Create and fit KMeans model to data for a variety of k's
-    distortions = [] 
-    inertias = [] 
-    mapping1 = {} 
-    mapping2 = {} 
-    K = range(1,10) 
-  
-    for k in K: 
-        #Building and fitting the model 
-        kmeanModel = KMeans(n_clusters=k).fit(potentialClientsDF) 
-        kmeanModel.fit(potentialClientsDF)     
-      
-        distortions.append(sum(np.min(cdist(potentialClientsDF, kmeanModel.cluster_centers_, 
-                          'euclidean'),axis=1)) / potentialClientsDF.shape[0]) 
-        inertias.append(kmeanModel.inertia_) 
-  
-        mapping1[k] = sum(np.min(cdist(potentialClientsDF, kmeanModel.cluster_centers_, 
-                     'euclidean'),axis=1)) / potentialClientsDF.shape[0] 
-        mapping2[k] = kmeanModel.inertia_ 
+    #Calculate and print empirical method result for ideal number of clusters
+    print("\nEmpirical Method # Clusters:   {0}".format(math.sqrt(potentialClientsDF.shape[0]/2)))
 
-    plt.plot(K, distortions, 'bx-') 
-    plt.xlabel('Values of K') 
-    plt.ylabel('Distortion') 
-    plt.title('The Elbow Method using Distortion') 
-    plt.show() 
+    #Create and fit KMeans model to data for a variety of k's, collecting distortion values for each
+    #plot variance for each value for 'k' between 1,10
+    initial = [cluster.vq.kmeans(potentialClientsDF,i) for i in range(1,10)]
+    plt.title('The Elbow Plot for Potential Clients')
+    plt.plot([var for (cent,var) in initial])
+    plt.show()
 
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(potentialClientsDF)
+    cent, var = initial[3]
+    #use vq() to get as assignment for each obs.
+    assignment,cdist = cluster.vq.vq(potentialClientsDF,cent)
+    plt.scatter(potentialClientsDF.iloc[:,0], potentialClientsDF.iloc[:,1], c=assignment)
+    plt.title('The Data Partitioned into 4 Clusters')
+    plt.show()
+
+    #Elbow plot and kmeans cluster plot implementations borrowed from: https://stats.stackexchange.com/questions/9850/how-to-plot-data-output-of-clustering
